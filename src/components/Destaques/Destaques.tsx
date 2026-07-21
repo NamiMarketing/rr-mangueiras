7ab42a9f8d7a5627f7b43de07dfa6aa4c03c7e6f
Marcelo <marcelo.cho@gmail.com>
2026-07-21 16:07:37 -0300

    removed inox from destaques

--- src/components/Destaques/Destaques.tsx
+++ src/components/Destaques/Destaques.tsx
@@ -13,7 +13,6 @@ import pneumatica from "@/img/destaques/pneumatica.png";
 import galvanizado from "@/img/destaques/galvanizado.png";
 import mangueira from "@/img/destaques/mangueira.png";
 import engates from "@/img/destaques/engates.png";
-import inox from "@/img/destaques/inox.png";
 import vonixx from "@/img/destaques/vonixx.png";
 
 const destaquesData = [
@@ -44,11 +43,6 @@ const destaquesData = [
   },
   {
     id: 6,
-    title: "INOX",
-    image: inox,
-  },
-  {
-    id: 7,
     title: "VONIXX",
     image: vonixx,
   },
